eb4cae21176c51a3c8ba1fc933135309e88eddf4
agustin.perez <perezp.agustin@gmail.com>
2020-07-13 04:41:24 -0300

    Integrated react reveal to adding fade animations

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Cat from '../../components/cat/Cat'
+import { Fade } from 'react-reveal'
+import './Home.css'
 
 const Home = ({history}) => {
 	return (
-		<div>
-			<Cat history={history}/>
-			<label>Hola Humano!</label>
+		<div className='home-catbot-container'>
+			<div className='home-catbot-content'>
+				<Cat history={history}/>
+				<div className='home-catbot-greeting'>
+					<Fade opposite>
+						<h1>Hi Human!</h1>
+					</Fade>
+					<Fade opposite>
+						<label>Do you want to chat?</label>
+						<label>Click me to start :)</label>
+					</Fade>
+				</div>
+			</div>
 		</div>
 	)
 }
